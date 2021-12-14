7590a23a8ec7b7b84eccdf6cf2d1fc55195d0d2d
ramhors <rjm2075@gmail.com>
2021-12-14 18:03:20 -0500

    finishing the user

--- cookbook/src/main/java/controller/Controller.java
+++ cookbook/src/main/java/controller/Controller.java
@@ -1,5 +1,19 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import service.UserService;
+
+@RestController
 public class Controller {
 
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping(path = "/demo")
+	public String test() {
+		return "It's working";
+	}
 }

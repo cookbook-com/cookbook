813d447ec0a6002804c3bee4165f34bc2cbc58e0
marshall-hobbs96 <marshall.hobbs96@gmail.com>
2021-12-17 13:57:28 -0600

    Got really basically functioning recipe detailed component

---
+++ frontend/src/app/Recipe.ts
@@ -0,0 +1,10 @@
+export interface Recipe {
+
+    idMeal: number,
+    strMeal: string,
+    strIngredient1: string,
+    strIngredient2: string,
+    strIngredient3: string,
+    strInstructions: string,
+    strMealThumb: string
+}

b84159e45da646048a9d32af4c5bea8f6a36bb68
Rafael Dantas <rafambd@gmai.com>
2021-12-18 23:51:48 -0500

    updated main gitignore to include properties of angular gitgnore, as well as started working on our angular project

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { Recipe } from '../Recipe';
+import { RecipeService } from '../recipe.service';
+//import { RecipeSearchService } from '../recipe.service'; // maybe replace later with a search service, if we make one EDIT: not needed since we get put all of our methods in recipe.ts
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +11,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  recipes$!: Observable<Recipe[]>;
+  private searchTerms = new Subject<string>(); // a subject is both a source of observable values and an Observable itself. It can be subscribed the same way.
+  
+  constructor(private recipeService: RecipeService) { }
+  // Push a search term into the observable stream.
+  search(term: string): void{
+    this.searchTerms.next(term);
+  }
 
   ngOnInit(): void {
+    this.recipes$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(300),
+
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.recipeService.searchHeroes(term)),
+    );
   }
 
 }
--- frontend/src/app/recipe.service.ts
+++ frontend/src/app/recipe.service.ts
@@ -6,6 +6,14 @@ import { Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class RecipeService {
+  searchHeroes(term: string): any {
+    throw new Error('Method not implemented.');
+  }
 
-  constructor() { }
+  private recipeUrl = 'api/recipe'; //URL to web api, form of :base/collectionName.
+
+  constructor(private http: HttpClient) { // if we're doing a message thing like in Tour of Heroes, include one here
+
+  }
+  // a lot of our Http related code will go here ex: GET recipe, user, etc, if following a structure to Tour of Heroes. 
 }

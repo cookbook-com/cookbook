7a9204bce01d99fed15fa7c7dd58991e64976da0
Rafael Dantas <rafambd@gmai.com>
2021-12-21 00:07:54 -0500

    Finished a lot of the work on Nav bar, need to add styling and figure searching out, added/ commented on stuff in recipe service

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -14,14 +14,31 @@ export class NavbarComponent implements OnInit {
   recipes$!: Observable<Recipe[]>;
   private searchTerms = new Subject<string>(); // a subject is both a source of observable values and an Observable itself. It can be subscribed the same way.
   
+  //userImageLink = "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcliparts.co%2Fcliparts%2Fdc4%2Foyn%2Fdc4oyngoi.jpg&f=1&nofb=1";
+  //logoImageLink ="https://i.pinimg.com/600x315/22/d8/c6/22d8c6d951616262123aa3e7229d7cbc.jpg";
+
   constructor(private recipeService: RecipeService) { }
   // Push a search term into the observable stream.
   search(term: string): void{
     this.searchTerms.next(term);
   }
 
+  filterOnToggle: boolean = false;
+  toggleFilters(){
+    // https://github.com/211018jwa/training/tree/main/week-9/mock-interview-angular-projects/group-one-app/src/app
+    // toggle filter to show/ hide navbar
+    this.filterOnToggle = !this.filterOnToggle
+  }
+  getRandomRecipe(){
+    this.recipeService.getRandomRecipe();
+  }
+  /* WIP
+  searchIngredient(term: string): void{
+    this.searchTerms.next(term);
+  }
+  */
   ngOnInit(): void {
-    /*this.recipes$ = this.searchTerms.pipe(
+    this.recipes$ = this.searchTerms.pipe(
       // wait 300ms after each keystroke before considering the term
       debounceTime(300),
 
@@ -29,8 +46,8 @@ export class NavbarComponent implements OnInit {
       distinctUntilChanged(),
 
       // switch to new search observable each time the term changes
-      switchMap((term: string) => this.recipeService.searchHeroes(term)),
-    );*/
+      switchMap((term: string) => this.recipeService.searchRecipes(term)),
+    );
   }
 
 }

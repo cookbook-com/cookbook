1faebc3e31573dd0e5108c18b7ead2b6b681162d
marshall-hobbs96 <marshall.hobbs96@gmail.com>
2021-12-22 15:13:43 -0600

    Enables cors on backend controllers. Created method stubs for all unit tests. Finished unit tests for UserDao. Halfway through unit tests for UserService

--- cookbook/src/test/java/com/revature/cookbook/daounittests/CookbookDaoTests.java
+++ cookbook/src/test/java/com/revature/cookbook/daounittests/CookbookDaoTests.java
@@ -1,5 +1,65 @@
 package com.revature.cookbook.daounittests;
 
+import javax.persistence.EntityManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.revature.cookbook.dao.CookbookDao;
+
+@SpringBootTest
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 public class CookbookDaoTests {
 
+	
+	@Autowired 
+	private EntityManager em;
+	
+	@Autowired
+	private CookbookDao cookbookDaoSut; 
+	
+	@Test
+	@Transactional
+	public void createCookbookPositive() {
+		
+		
+		
+	}
+	
+	@Test
+	@Transactional
+	public void getCookbookByIdPositive() {
+		
+		
+		
+	}
+	
+	@Test
+	@Transactional
+	public void getCookbookByIdNoCookbookWithIdNegative() {
+		
+		
+		
+	}
+	
+	@Test
+	@Transactional
+	public void addRecipeToCookbookPositive() {
+		
+		
+		
+	}
+	
+	@Test
+	@Transactional
+	public void addRecipeToCookbookNoCookbookWithIdNegative() {
+		
+		
+		
+	}
+	
 }
--- cookbook/src/test/java/com/revature/cookbook/serviceunittests/CookbookServiceTests.java
+++ cookbook/src/test/java/com/revature/cookbook/serviceunittests/CookbookServiceTests.java
@@ -1,5 +1,92 @@
 package com.revature.cookbook.serviceunittests;
 
+import javax.annotation.Resource;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.revature.cookbook.dao.CookbookDao;
+import com.revature.cookbook.service.CookbookService;
+
+@SpringBootTest
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 public class CookbookServiceTests {
 
+	@Mock
+	private CookbookDao mockCookbookDao; 
+	
+	@InjectMocks
+	@Resource
+	private CookbookService cookbookServiceSut;
+	
+	@BeforeEach
+	public void setup() {
+		
+		MockitoAnnotations.openMocks(this);
+		
+	}
+	
+	@Test
+	@Transactional
+	public void getCookbookWithIdPositive() {
+		
+		
+		
+	}
+	
+	@Test
+	@Transactional
+	public void getCookbookWithIdCookbookDoesntExistNegative() {
+		
+		
+		
+	}
+	
+	@Test
+	@Transactional
+	public void addRecipeToCookbookPositive() {
+		
+		
+		
+	}
+	
+	
+	@Test
+	@Transactional
+	public void addRecipeToCookbookCookbookDoesntExistNegative() {
+		
+		
+		
+	}
+	
+	@Test
+	@Transactional
+	public void createCookbookPositive() {
+		
+		
+		
+	}
+	
+	@Test
+	@Transactional
+	public void createCookbookNameTooLongNegative() {
+		
+		
+		
+	}
+	
+	@Test
+	@Transactional
+	public void createCookbookUserDoesntExist() {
+		
+		
+		
+	}
 }
